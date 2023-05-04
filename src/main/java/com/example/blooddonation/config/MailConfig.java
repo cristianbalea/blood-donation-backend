d7656213c717aaa451df1504db3d42d5f6f30f57
Cristian Balea <127232514+cristianbalea@users.noreply.github.com>
2023-05-04 19:23:19 +0300

    Update MailConfig.java

--- src/main/java/com/example/blooddonation/config/MailConfig.java
+++ src/main/java/com/example/blooddonation/config/MailConfig.java
@@ -17,7 +17,7 @@ public class MailConfig {
         mailSender.setPort(587);
 
         mailSender.setUsername("blood.donation.demo@gmail.com");
-        mailSender.setPassword("kpffcvyaxjlmplhb");
+        mailSender.setPassword("");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
